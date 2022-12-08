b1a66175ca5e7b8f5dc34f67ee92107a08f94cdd
Johan Litaudon <118103893+Jintow@users.noreply.github.com>
2022-12-09 00:26:19 +0100

    idee de recursivites pour l'algorithme2

--- Push-Swap42/src/algo_sorting.c
+++ Push-Swap42/src/algo_sorting.c
@@ -6,7 +6,7 @@
 /*   By: Teiki <Teiki@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 23:55:36 by Teiki             #+#    #+#             */
-/*   Updated: 2022/12/09 00:22:27 by Teiki            ###   ########.fr       */
+/*   Updated: 2022/12/09 00:26:10 by Teiki            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	left_sorting(t_listi **la, t_listi **lb, int i_piv, int low_piv)
 		check_sort + pre_sort;
 		sort_three(lb, 'b');
 		if (low_piv < 1)
-			right_sorting(la, lb, i_piv, low_piv + 1)
+			right_sorting(la, lb, i_piv, low_piv + 1) //+ possibilites de double rev_rot
 		else if (*lb)
 			left_sorting(la, lb, i_piv + 1, 0)
 		else
@@ -58,7 +58,7 @@ void	right_sorting(t_listi **la, t_listi **lb, int i_piv, int low_piv)
 	else
 	{
 		split_categ_sorting(la, i_piv, low_piv, find_max_low_categ(la, i_piv, low_piv));
-		pivot_right(lb, i_piv, low_piv + 1);
+		pivot_right(lb, i_piv, low_piv + 1); //+possibilites de double rev_rot, a priori non!
 		left_sorting(la, lb, i_piv, low_piv + 1);
 	}
 }
